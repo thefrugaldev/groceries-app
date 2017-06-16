f6e279d0cdbf10b759055c4e48cab01428dc8c46
Clayton Orman <corman.dev@gmail.com>
2017-06-16 17:23:22 -0400

    Completed Chapter 3.4

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,47 @@
 import { Component } from "@angular/core";
+import { User } from "./shared/user/user";
+import { UserService } from "./shared/user/user.service";
 
 @Component({
   selector: "my-app",
-  template: `
-    <StackLayout>
-      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-
-      <TextField hint="Email Address" keyboardType="email" autocorrect="false" autocapitalizationType="none"></TextField>
-      <TextField hint="Password" secure="true"></TextField>
-
-      <Button text="Sign in" class="submit-button"></Button>
-      <Button text="Sign up for Groceries"></Button>    
-    <StackLayout>
-  `,
+  providers: [UserService],
+  templateUrl: "./pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+
+export class AppComponent {
+  user: User;
+  isLoggingIn = true;
+
+  constructor(private userService: UserService) {
+    this.user = new User();
+    this.user.email = "corman.dev@gmail.com";
+    this.user.password = "Welcome1";
+  }
+
+  submit() {
+    if(this.isLoggingIn)
+      this.login();
+    else
+      this.signUp();
+  }
+
+  login() {
+    //TODO: Define
+  }
+
+  signUp() {
+    this.userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was created successfully.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately, we were unable to create your account.")
+      );
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from "@angular/core";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [NativeScriptModule],
+  imports: [
+    NativeScriptModule,
+    NativeScriptFormsModule,
+    NativeScriptHttpModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
