eadbe868521063d012e736b99d6e77294150b6b5
Clayton Orman <corman.dev@gmail.com>
2017-06-20 17:10:40 -0400

    Completed Chapter 6.2

--- app/utils/hint-util.ts
+++ app/utils/hint-util.ts
@@ -6,9 +6,14 @@ declare var NSDictionary: any;
 declare var NSForegroundColorAttributeName: any;
 
 export function setHintColor(args: { view: TextField, color: Color }) {
-    let dictionary = new NSDictionary(
-        [args.color.ios],
-        [NSForegroundColorAttributeName]
-    );
-    args.view.ios.attributedPlaceholder = NSAttributedString.alloc().initWithStringAttributes(args.view.hint, dictionary);
+    if (args.view.android) {
+        args.view.android.setHintTextColor(args.color.android);
+    }
+    if (args.view.ios){
+        let dictionary = new NSDictionary(
+            [args.color.ios],
+            [NSForegroundColorAttributeName]
+        );
+        args.view.ios.attributedPlaceholder = NSAttributedString.alloc().initWithStringAttributes(args.view.hint, dictionary);  
+    }    
 }
